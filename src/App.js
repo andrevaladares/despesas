bc93a2311fe190f34e948b8a9c6747383ae97311
Valada <andre.valadares@gmail.com>
2022-01-22 17:46:37 -0300

    Pequenos ajustes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-import React from 'react'
 import CadastroDespesa from './componentes/CadastroDespesa'
 import ResumoDespesasMes from './componentes/ResumoDespesasMes';
 
