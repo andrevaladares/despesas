4b3f9867f46c4b1dda0a5c8c9c9d1a5d82e0e8f1
Valada <andre.valadares@gmail.com>
2022-01-22 18:46:43 -0300

    Pequenos ajustes

--- src/componentes/CadastroDespesa.jsx
+++ src/componentes/CadastroDespesa.jsx
@@ -1,8 +1,26 @@
+import { TextField } from '@material-ui/core'
+import React from 'react'
 
 function CadastroDespesa() {
 
     return (
-        <p>Cadastro de Despesa</p>
+        <>
+            <p>Cadastro de Despesa</p>
+            <form>
+                <TextField
+                    id="data"
+                    label="Data (dd/mm/aaaa)"
+                    type="text"
+                    variant="outlined"
+                    fullWidth
+                    margin="normal"
+                    format="DD/MM/YYYY"
+                />
+
+            </form>
+
+        </>
+
     )
 }
 
